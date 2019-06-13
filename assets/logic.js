56f3207e3446dda2decad58a5c0d82ba54fda99a
Charlie Parsons <cdaltonparsons@gmail.com>
2019-06-13 17:31:44 -0600

    finished the HTML skeleton, and began the logic/firebase portion

---
+++ assets/logic.js
@@ -0,0 +1,61 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyB4eSfu8ODYM89uzCL2bM_TCIaDSmcPCEA",
+  authDomain: "my-awesome-project-ed513.firebaseapp.com",
+  databaseURL: "https://my-awesome-project-ed513.firebaseio.com",
+  projectId: "my-awesome-project-ed513",
+  storageBucket: "my-awesome-project-ed513.appspot.com",
+  messagingSenderId: "848683298775",
+  appId: "1:848683298775:web:a49923dba1ecc8bb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var firstTrain = "";
+var frequency = "";
+
+$("#button").on("click", function(event) {
+  event.preventDefault();
+
+  trainName = $("#train-name")
+    .val()
+    .trim();
+  destination = $("#destination")
+    .val()
+    .trim();
+  firstTrain = $("#time-first-train")
+    .val()
+    .trim();
+  frequency = $("#frequency")
+    .val()
+    .trim();
+
+  database.ref().push({
+    train: trainName,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency
+  });
+});
+
+database.ref().on("child_added", function(snapshot) {
+  var sv = snapshot.val();
+  var tableRow = $("<tr>");
+  var newTrain = $("<td>").text(sv.train);
+  var newDestination = $("<td>").text(sv.destination);
+  var newFrequency = $("<td>").text(sv.frequency);
+  var nextArrival = $("<td>").text("");
+  var minutesAway = $("<td>").text("");
+
+  tableRow.append(
+    newTrain,
+    newDestination,
+    newFrequency,
+    nextArrival,
+    minutesAway
+  );
+  $("#table").append(tableRow);
+});
